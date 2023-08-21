3b16087a82571c1f4fce569474c9fdce61f0db09
Mohammed Ameer Hamza <101882275+Hamz1Ameer@users.noreply.github.com>
2023-08-21 21:13:21 +0530

    Create index.js

---
+++ Chapter-04/Functions/index.js
@@ -0,0 +1,41 @@
+// //Functions:
+// function sayHello() {
+//   return `Hello World`;
+// }
+// console.log(sayHello());
+//
+
+// Add numbers :
+// function addNumbers(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(addNumbers(10, 20));
+// console.log(addNumbers(22, 54));
+// console.log(addNumbers(100, -50));
+
+// function sub(num1, num2) {
+//   return num1 - num2;
+//   console.log(num1, num2); // after retun lines are not executed
+// }
+// console.log(sub(10, 4));
+// console.log(sub(5, -20));
+
+// arguments  and parameter :
+// function regerterUser(user = "Bot") {
+//   return user + ` is registered`;
+// }
+// console.log(regerterUser());
+
+//
+
+//rest params:
+
+// function sum(...numbers) {
+//   let total = 0;
+//   for (const num of numbers) {
+//     total += num;
+//   }
+//   return total;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5, 6, 100));
