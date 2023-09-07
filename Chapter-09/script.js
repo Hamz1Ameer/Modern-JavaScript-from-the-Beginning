0b554f4b94835d16c5173d8b7aae47e687d79cc8
Vivek <abhii.innovapath@gmail>
2023-09-07 15:35:45 -0700

    Added functionality to add new Item. When clicked on ADD ITEM button

---
+++ Chapter-09/script.js
@@ -0,0 +1,42 @@
+const itemList= document.getElementById('item-list');
+const itemInput= document.getElementById('item-input');
+const itemForm= document.getElementById('item-form');
+
+const addItem=(event)=>{
+    event.preventDefault();
+    let newItem= itemInput.value;
+
+    if(newItem ==''){
+        alert('Please add an Item');
+        return;
+    }
+
+    const li= document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+    // console.log(li);
+
+    
+    const createButton=(classes)=>{
+        const button = document.createElement("button");
+        button.className=classes;
+        const icon= createIcon("fa-solid fa-xmark")
+        button.appendChild(icon)
+        return button;
+    }
+    const createIcon=(classes)=>{
+        const icon = document.createElement("i");
+        icon.className=classes;
+        // console.log(icon);
+        return icon;    
+    }
+
+    const button =createButton("remove-item btn-link text-red");
+    // console.log(button);
+    li.appendChild(button);
+
+    itemList.appendChild(li)
+    itemInput.value ='';
+}
+
+
+itemForm.addEventListener('submit', addItem)
