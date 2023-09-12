285f09a66e364d42d2379517e8997757e2e981ff
Hamz1Ameer <ameerhamza7702@gmail.com>
2023-09-12 15:53:32 -0700

    Added filter functionality

--- Chapter-09/script.js
+++ Chapter-09/script.js
@@ -1,56 +1,89 @@
-const itemList= document.getElementById('item-list');
-const itemInput= document.getElementById('item-input');
-const itemForm= document.getElementById('item-form');
-const clearBtn= document.getElementById('clear');
+const itemList = document.getElementById("item-list");
+const itemInput = document.getElementById("item-input");
+const itemForm = document.getElementById("item-form");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
-const addItem=(event)=>{
-    event.preventDefault();
-    let newItem= itemInput.value;
+const addItem = (event) => {
+  event.preventDefault();
+  let newItem = itemInput.value;
 
-    if(newItem ==''){
-        alert('Please add an Item');
-        return;
+  if (newItem == "") {
+    alert("Please add an Item");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+  // console.log(li);
+
+  const createButton = (classes) => {
+    const button = document.createElement("button");
+    button.className = classes;
+    const icon = createIcon("fa-solid fa-xmark");
+    button.appendChild(icon);
+    return button;
+  };
+  const createIcon = (classes) => {
+    const icon = document.createElement("i");
+    icon.className = classes;
+    // console.log(icon);
+    return icon;
+  };
+
+  const button = createButton("remove-item btn-link text-red");
+  // console.log(button);
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+  checkUI();
+  itemInput.value = "";
+};
+
+const removeItem = (event) => {
+  if (event.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+      event.target.parentElement.parentElement.remove();
     }
+  }
+  checkUI();
+};
 
-    const li= document.createElement('li');
-    li.appendChild(document.createTextNode(newItem));
-    // console.log(li);
-    
-    const createButton=(classes)=>{
-        const button = document.createElement("button");
-        button.className=classes;
-        const icon= createIcon("fa-solid fa-xmark")
-        button.appendChild(icon)
-        return button;
+const filterItem = (e) => {
+  const items = document.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+  // console.log(e.target.value);
+  items.forEach((item) => {
+    const itemName=item.firstChild.textContent.toLowerCase();
+    if(itemName.indexOf(text)!=-1){
+        item.style.display= 'flex'
     }
-    const createIcon=(classes)=>{
-        const icon = document.createElement("i");
-        icon.className=classes;
-        // console.log(icon);
-        return icon;    
+    else{
+        item.style.display='none'
     }
+  });
+};
 
-    const button =createButton("remove-item btn-link text-red");
-    // console.log(button);
-    li.appendChild(button);
+const clearItem = () => {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+};
 
-    itemList.appendChild(li)
-    itemInput.value ='';
+function checkUI() {
+  const items = document.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
 }
 
-const removeItem=(event)=>{
-console.log(event.target.parentElement.classList)
-if(event.target.parentElement.classList.contains('remove-item')){
-       event.target.parentElement.parentElement.remove()
-}
-}
-
-const clearItem=()=>{
-    while (itemList.firstChild){
-        itemList.removeChild(itemList.firstChild)
-    }
-
-}
-itemForm.addEventListener('submit', addItem)
-itemList.addEventListener('click', removeItem)
-clearBtn.addEventListener('click', clearItem)
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItem);
+itemFilter.addEventListener("input", filterItem);
+checkUI();
